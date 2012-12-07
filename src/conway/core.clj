1aaa08f614418ef37361f26bfec900f993a9b3c4
Daniel De Aguiar <ddeaguiar@pointslope.com>
2012-12-06 23:32:12 -0500

    Cell has 8 neighbors, not 4. Added next generation support.

--- src/conway/core.clj
+++ src/conway/core.clj
@@ -1,7 +1,48 @@
 (ns conway.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn populate-board
+  "Populates the board with the specified cells."
+  [board & living]
+  (if (empty? living) board
+                      (apply populate-board (assoc-in board
+                                                      (first living)
+                                                      1)
+                             (rest living))))
+
+(defn create-row
+  "Creates an empty row of col columns."
+  [col]
+  (vec (repeat col 0)))
+
+(defn create-board
+  "Creates an empty rows x col board."
+  [rows col]
+  (vec (repeat rows (create-row col))))
+
+(defn neighbors
+  "Returns the state of the given cell's neighbors."
+  [board cell]
+  (let [left     #(vector (first %) (- (second %) 1))
+        top      #(vector (- (first %) 1) (second %))
+        right    #(vector (first %) (+ (second %) 1))
+        bottom   #(vector (+ (first %) 1) (second %))
+        lt       (left (top cell))
+        t        (top cell)
+        tr       (top (right cell))
+        r        (right cell)
+        br       (bottom (right cell))
+        b        (bottom cell)
+        bl       (bottom (left cell))
+        l        (left cell)
+        gets #(get-in board % 0)]
+    [(gets lt) (gets t) (gets tr) (gets r) (gets br) (gets b) (gets bl) (gets l)]))
+
+(defn cell-state
+  "Returns cell state based on state of neighbors and cell state."
+  [cell-state cell-neighbors]
+  (let [count-live-neighbors (count (filter #(= 1 %) cell-neighbors))]
+    (cond (> 2 count-live-neighbors) 0
+          (and (= 1 cell-state) (>= 3 count-live-neighbors)) 1
+          (and (= 0 cell-state) (== 3 count-live-neighbors)) 1
+          :else 0)))
--- test/conway/core_test.clj
+++ test/conway/core_test.clj
@@ -4,45 +4,8 @@
 
 (unfinished )
 
-(defn populate-board
-  "Populates the board with the specified cells."
-  [board & living]
-  (if (empty? living) board
-                      (apply populate-board (assoc-in board
-                                                      (first living)
-                                                      1)
-                             (rest living))))
-
-(defn create-row
-  "Creates an empty row of col columns."
-  [col]
-  (vec (repeat col 0)))
-
-(defn create-board
-  "Creates an empty rows x col board."
-  [rows col]
-  (vec (repeat rows (create-row col))))
-
-(defn neighbors
-  "Returns the state of the given cell's neighbors."
-  [board cell]
-  (let [l (vector (first cell) (- (last cell) 1))
-        t (vector (- (first cell) 1) (last cell))
-        r (vector (first cell) (+ (last cell) 1))
-        b (vector (+ (first cell) 1) (last cell))
-        gets #(get-in board % 0)]
-    [(gets l) (gets t) (gets r) (gets b)]))
-
-(defn cell-state
-  "Returns cell state based on state of neighbors."
-  [n]
-  (let [live (count (filter #(= 1 %) n))]
-    (cond (> 2 live) 0
-          (>= 3 live) 1
-          :else 0)))
-
-
 (def four-x-four (partial populate-board (create-board 4 4)))
+(def five-x-five (partial populate-board (create-board 5 5)))
 
 (fact "A 4 column row."
   (create-row 4) => [0 0 0 0])
@@ -59,37 +22,66 @@
                                             [0 0 0 1]
                                             [0 0 0 1]])
 
-(fact "A cell has four neighbors."
-  (count (neighbors four-x-four [0 0])) => 4)
+(fact "A cell has eight neighbors."
+  (count (neighbors four-x-four [0 0])) => 8)
 
 (fact "Two neighbors are alive."
-  (neighbors [[0 1 0 0]
-              [0 0 1 0]
+  (neighbors [[1 0 0 0]
               [0 0 0 0]
+              [0 0 1 0]
               [0 0 0 0]]
-             [1 1]) => [0 1 1 0])
+             [1 1]) => [1 0 0 0 1 0 0 0])
 
 (fact "All neighbors are dead."
-  (neighbors [[0 1 0 0]
-              [0 0 1 0]
+  (neighbors [[0 0 0 0]
+              [0 0 0 0]
               [0 0 0 0]
               [0 0 0 0]]
-             [1 0]) => [0 0 0 0])
+             [1 0]) => [0 0 0 0 0 0 0 0])
 
 (fact "All neighbors are alive."
-  (neighbors [[0 1 0 0]
+  (neighbors [[1 1 1 0]
               [1 0 1 0]
-              [0 1 0 0]
+              [1 1 1 0]
               [0 0 0 0]]
-             [1 1]) => [1 1 1 1])
+             [1 1]) => [1 1 1 1 1 1 1 1])
 
-(fact "Any cell with fewer than two live neighbors dies due to under population."
-  (cell-state [0 0 0 0]) => 0
-  (cell-state [1 0 0 0]) => 0)
+(fact "Any live cell with fewer than two live neighbors dies due to under population."
+  (cell-state 1 [0 0 0 0 0 0 0 0]) => 0
+  (cell-state 1 [1 0 0 0 0 0 0 0]) => 0)
 
-(fact "Any cell with two to three live neighbors lives on to the next generation."
-  (cell-state [1 1 0 0]) => 1
-  (cell-state [1 1 1 0]) => 1)
+(fact "Any live cell with two to three live neighbors lives on to the next generation."
+  (cell-state 1 [1 1 0 0 0 0 0 0]) => 1
+  (cell-state 1 [1 1 1 0 0 0 0 0]) => 1)
 
-(fact "Any cell with more than three live neighbors dies due to overcrowding"
-  (cell-state [1 1 1 1]) => 0)
+(fact "Any live cell with more than three live neighbors dies due to overcrowding"
+  (cell-state 1 [1 1 1 1 1 1 1 1]) => 0)
+
+(fact "A dead cell with exactly three live neighbors lives as if reproduction occurred."
+  (cell-state 0 [1 1 1 0 0 0 0 0]) => 1
+  (cell-state 0 [1 1 0 0 0 0 0 0]) => 0
+  (cell-state 0 [1 1 1 1 0 0 0 0]) => 0
+  (cell-state 0 [1 1 1 1 1 1 1 1]) => 0
+  (cell-state 0 [0 0 0 0 0 0 0 0]) => 0
+  )
+
+;(def box fourxfour [1 1] [1 2] [2 1] [2 2])
+(def blinker [[0 0 0 0 0]
+              [0 0 1 0 0]
+              [0 0 1 0 0]
+              [0 0 1 0 0]
+              [0 0 0 0 0]])
+
+(defn next-gen
+  [board]
+  (map-indexed (fn [r-index row]
+                 (map-indexed #(cell-state %2 (neighbors board [r-index % %2]))
+                              row))
+               board))
+
+(fact "Blinker should 'blink' in next generation"
+  (next-gen blinker) => [[0 0 0 0 0]
+                         [0 0 0 0 0]
+                         [0 1 1 1 0]
+                         [0 0 0 0 0]
+                         [0 0 0 0 0]])
