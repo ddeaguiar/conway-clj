5d8b5bd44e999ed26adacb6d1259cd539c1697bf
Daniel De Aguiar <ddeaguiar@pointslope.com>
2012-12-06 21:54:00 -0500

    Added neighbor lookup.

--- test/conway/core_test.clj
+++ test/conway/core_test.clj
@@ -23,23 +23,53 @@
   [rows col]
   (vec (repeat rows (create-row col))))
 
+(defn neighbors
+  "Returns the state of the given cell's neighbors."
+  [board cell]
+  (let [l (vector (first cell) (- (last cell) 1))
+        t (vector (- (first cell) 1) (last cell))
+        r (vector (first cell) (+ (last cell) 1))
+        b (vector (+ (first cell) 1) (last cell))
+        gets #(get-in board % 0)]
+    [(gets l) (gets t) (gets r) (gets b)]))
+
+(def four-x-four (partial populate-board (create-board 4 4)))
+
 (fact "A 4 column row."
   (create-row 4) => [0 0 0 0])
 
 (fact "A 4x4 board."
-  (create-board 4 4) => [[0 0 0 0]
-                         [0 0 0 0]
-                         [0 0 0 0]
-                         [0 0 0 0]])
+  (create-board 4 4) => (four-x-four))
 
 (fact "An empty board populated with no living cells is empty."
-  (populate-board (create-board 4 4)) => [[0 0 0 0]
-                                          [0 0 0 0]
-                                          [0 0 0 0]
-                                          [0 0 0 0]])
+  (populate-board (create-board 4 4)) => (four-x-four))
 
 (fact "An empty board should be populated with living cells."
-  (populate-board (create-board 4 4) [0 1] [1 2] [2 3] [3 3]) => [[0 1 0 0]
-                                                                  [0 0 1 0]
-                                                                  [0 0 0 1]
-                                                                  [0 0 0 1]])
+  (four-x-four [0 1] [1 2] [2 3] [3 3]) => [[0 1 0 0]
+                                            [0 0 1 0]
+                                            [0 0 0 1]
+                                            [0 0 0 1]])
+
+(fact "A cell has four neighbors."
+  (count (neighbors four-x-four [0 0])) => 4)
+
+(fact "Two neighbors are alive."
+  (neighbors [[0 1 0 0]
+              [0 0 1 0]
+              [0 0 0 0]
+              [0 0 0 0]]
+             [1 1]) => [0 1 1 0])
+
+(fact "All neighbors are dead."
+  (neighbors [[0 1 0 0]
+              [0 0 1 0]
+              [0 0 0 0]
+              [0 0 0 0]]
+             [1 0]) => [0 0 0 0])
+
+(fact "All neighbors are alive."
+  (neighbors [[0 1 0 0]
+              [1 0 1 0]
+              [0 1 0 0]
+              [0 0 0 0]]
+             [1 1]) => [1 1 1 1])
